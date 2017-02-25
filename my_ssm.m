8ba0888edcc6e8c582883b6eb525be87d30fa36b
Daniel Balcells <dbalcells@gmail.com>
2017-02-25 18:47:14 +0100

    Allowing specific transpositionsns to be defined

--- my_ssm.m
+++ my_ssm.m
@@ -1,8 +1,9 @@
-function ssm = my_ssm(feats, transpInvariant)
+function ssm = my_ssm(feats, allowedTranspositions)
 % function ssm = my_ssm(feats, transpInvariant)
 %
 % Compute self-similarity matrix between vectors of pitch class 
-% distributions. Optionally, do this in a transposition-invariant way.
+% distributions. Optionally, do this in a transposition-invariant way,
+% specifying what transpositions in semitones are allowed.
 
     % Compute dimensions and preallocate memory
     dims = size(feats);
@@ -10,13 +11,20 @@ function ssm = my_ssm(feats, transpInvariant)
     nFeats = dims(2);
     ssm = zeros(nVects,nVects);
     
+    if sum(allowedTranspositions ~= 0) ~= 0
+        transpInvariant = 1;
+    else
+        transpInvariant = 0;
+    end
+    
     % Iterate vectors and compute pair-wise cosine distances.
     for i=1:nVects
         a = feats(i,:);
         for j=1:nVects
             b = feats(j,:);
             if transpInvariant
-                ssm(i,j) = transpositionInvariantCosineDistance(a,b);
+                ssm(i,j) = transpositionInvariantCosineDistance(a,b,...
+                    allowedTranspositions);
             else
                 ssm(i,j) = dot(a,b)/ (sqrt(sum(a.*2)) * sqrt(sum(b.*2)));
             end
--- transpositionInvariantCosineDistance.m
+++ transpositionInvariantCosineDistance.m
@@ -1,19 +1,20 @@
-function dist = transpositionInvariantCosineDistance(a,b)
+function dist = transpositionInvariantCosineDistance(a,b,allowedShifts)
 % function dist = transpositionInvariantCosineDistance(a,b)
 %
 % Compute transposition-invariant cosine distance between pitch class
-% distribution vectors a and b. Currently, this is done by taking the cosine
-% distance between a and every possible circular shift of b and returning
-% the minimum of these distances.
+% distribution vectors a and b. Allowed
+% Currently, this is done by taking the cosine
+% distance between a and every allowed circular shift of b and returning
+% the maximum of these distances.
     dims = size(a);
     nFeats = dims(2);
-    shiftDists = zeros(1,nFeats);
+    nShifts = length(allowedShifts);
+    shiftDists = zeros(1,nShifts);
     
-    %Iterate all possible shifts and get distance for each shift
-    c = b;
-    for i=1:nFeats
+    %Iterate all allowed shifts and get distance for each shift
+    for i=1:nShifts
+        c = circshift(b,allowedShifts(i));
         shiftDists(i) = dot(a,c) / (sqrt(sum(a.*2)) * sqrt(sum(c.*2)));
-        c = circshift(c,1);
     end
     
     dist = max(shiftDists);
