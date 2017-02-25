ef7449b19fb8058f41f257f701638e48c707b128
Daniel Balcells <dbalcells@gmail.com>
2017-02-25 19:46:52 +0100

    Short function to get a sequence of keys estimated from a MIDI note matrix

---
+++ getKeySequence.m
@@ -0,0 +1,23 @@
+function keySequence = getKeySequence(nmat,wLen,hop)
+% function pitchClassesSequence = getPitchClassesSequence(nmat,wLen,hop)
+% 
+% Computes keys on a MIDI note matrix using a sliding
+% window of the given length and hopsize.
+
+    % Compute total number of output histograms and preallocate memory
+    offset = 0;
+    duration = nmat(end,6);
+    nWindows = ceil(duration/hop);
+    keySequence = zeros(nWindows,12);
+    
+    % Slide window over note matrix. Retrieve all MIDI events within the
+    % each window and compute pitch class distribution.
+    for i = 1:nWindows
+        thisWindowEvents = nmat(find((...
+            (nmat(:,6)-offset)<wLen) .* ...
+            ((nmat(:,6)-offset)>0)),:);
+        if ~isempty(thisWindowEvents)
+            keySequence(i,:) = kkkey(thisWindowEvents);
+        end
+        offset = offset + hop;
+    end
